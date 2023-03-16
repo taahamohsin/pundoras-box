0f4c9613221a7f855028b9ddd972b8f702b0ff9d
Taaha Mohsin <taaha.b.mohsin@vanderbilt.edu>
2023-03-15 22:48:44 -0400

    Make release-tasks.sh executable

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   );
 
   const transformDataByRank = (data) => {
-    data.sort((first, second) => second.score - first.score);
+    data.sort((first, second) => second.score ?? 0 - first.score ?? 0);
     const displayData = data.map(({imgUrl, ...rest}) => ({...rest}));
     return displayData.map((user, index) => ({
       key: index,
