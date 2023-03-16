079b93b8ec181a367750ad557aa3d4a8afdec3f1
Taaha Mohsin <taaha.b.mohsin@vanderbilt.edu>
2023-03-16 01:29:55 -0400

    Fixed release task

--- release_tasks.sh
+++ release_tasks.sh
@@ -1,3 +1,3 @@
-bin/rails db:create
+bin/rails db:create DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bin/rails db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bin/rails db:migrate DISABLE_DATABASE_ENVIRONMENT_CHECK=1
